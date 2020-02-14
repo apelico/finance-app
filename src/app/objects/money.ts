a41c7ed668cc4257bafb5e4a97cba91e005d21ec
apelico <austinopelico96@gmail.com>
2020-02-14 11:17:52 -0600

    1 file added

---
+++ src/app/objects/money.ts
@@ -0,0 +1,13 @@
+export class Money {
+  index: number;
+  id: number;
+
+  name : string = 'Name';
+  date : string = '';
+  day : number;
+  amount : number = 0;
+
+  isWeekly = false;
+  isBiWeekly = false;
+  isMonthly = false;
+}
